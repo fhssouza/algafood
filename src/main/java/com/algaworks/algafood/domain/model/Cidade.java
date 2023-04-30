1c493910c003cf17857ad0069ffbb3e5200e85ad
Fábio Souza <fhssouza@gmail.com>
2023-04-30 09:21:45 -0300

    3.20. Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Cidade {
+
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Grupo {
+
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
