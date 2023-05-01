af5d3c887b7c089fe6b8c6b1e5253ef9f2144b0f
Fábio Souza <fhssouza@gmail.com>
2023-05-01 09:38:40 -0300

    4.10. Modelando e requisitando um Collection Resource com GET

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface CozinhaRepository {
 
-    List<Cozinha> todas();
-    Cozinha porId(Long id);
-    Cozinha adicionar(Cozinha cozinha);
+    List<Cozinha> listar();
+    Cozinha buscar(Long id);
+    Cozinha salvar(Cozinha cozinha);
     void remover(Cozinha cozinha);
 
 }
