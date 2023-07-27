6ff5579f9c58e1bd3846cebbbb569fc688e52e81
Fábio Souza <fhssouza@gmail.com>
2023-07-27 18:14:34 -0300

    8.8. Criando a exception NegocioException

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
@@ -37,7 +38,11 @@ public class CidadeController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cidade adicionar(@RequestBody Cidade cidade){
-        return cadastroCidade.salvar(cidade);
+        try{
+            return cadastroCidade.salvar(cidade);
+        }catch (EntidadeNaoEncontradaException e){
+            throw new NegocioException(e.getMessage());
+        }
     }
 
     @PutMapping("/{cidadeId}")
@@ -46,7 +51,11 @@ public class CidadeController {
 
             BeanUtils.copyProperties(cidade, cidadeAtual, "id");
 
-            return cadastroCidade.salvar(cidadeAtual);
+            try {
+                return cadastroCidade.salvar(cidadeAtual);
+            }catch (EntidadeNaoEncontradaException e){
+                throw new NegocioException(e.getMessage());
+            }
     }
 
     @DeleteMapping("/{cidadeAId}")
