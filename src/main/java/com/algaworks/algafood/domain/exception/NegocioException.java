f92e11ac464b6ec401e52213b6fc16f65b6cb4ac
Fábio Souza <fhssouza@gmail.com>
2023-07-29 16:13:10 -0300

    8.14. Desafio: implementando exception handler

--- src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
@@ -1,9 +1,5 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class NegocioException extends RuntimeException {
     private static final Long serialVersionUID = 1L;
     public NegocioException(String message){
